e0124c54ef809fbfdc5fc587ca73302c40588fdd
Ahmed0484 <a_elshazly_08@hotmail.com>
2026-04-01 17:04:49 +0200

    shopping cart

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './assets/styles/index.css'
 import './assets/styles/bootstrap.custom.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
+import CartScreen from './screens/CartScreen.jsx'
 import App from './App.jsx'
 import {
   createBrowserRouter,
@@ -19,6 +20,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 );
---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
+
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [] };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      // The item to add to the cart
+      const item = action.payload;
+       // Update the cart state using the updateCart function
+      return updateCart(state, item);
+    },
+     removeFromCart: (state, action) => {
+      // Filter out the item to remove from the cart
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+      // Update the prices and save to storage
+      return updateCart(state);
+    }
+  },
+});
+
+export const { addToCart , removeFromCart} = cartSlice.actions;
+
+export default cartSlice.reducer;
